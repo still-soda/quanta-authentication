2088c472c06ac16e8e1fe5cc0f7f6f5d77fda86f
OrangeSoda <951040628@qq.com>
2026-01-08 12:08:46 +0800

    feat: 搭建 admin web 的基础框架

--- packages/qauth-admin-web/src/main.ts
+++ packages/qauth-admin-web/src/main.ts
@@ -1,5 +1,45 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import 'primeicons/primeicons.css';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from 'primevue/resources/themes/aura/theme.css';
+import { definePreset } from '@primeuix/themes';
 
-createApp(App).mount('#app')
+const getColorLevels = (color: string) => {
+   const colorLevels: Record<number, string> = {};
+   [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950].forEach((level) => {
+      colorLevels[level] = `{${color}.${level}}`;
+   });
+   return colorLevels;
+};
+
+const AppPreset = definePreset(Aura, {
+   semantic: {
+      primary: getColorLevels('orange'),
+      surface: {
+         0: '#ffffff',
+         ...getColorLevels('soho'),
+      },
+      highlight: {
+         background: '{primary.100}',
+         color: '{primary.700}',
+      },
+   },
+});
+
+createApp(App)
+   .use(PrimeVue, {
+      ripple: true,
+      theme: {
+         preset: AppPreset,
+         options: {
+            darkModeSelector: '.app-dark',
+            cssLayer: {
+               name: 'primevue',
+               order: 'theme, base, primevue',
+            },
+         },
+      },
+   })
+   .mount('#app');
